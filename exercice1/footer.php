cf6049b1157c84a8998f7a10da07143b731fb65e
Lenikpoho Ahmed KONE <47745603+koneahmed@users.noreply.github.com>
2023-02-13 11:13:13 +0000

    first commit

---
+++ exercice1/footer.php
@@ -0,0 +1,15 @@
+
+    <style type="text/css">
+                footer {
+        text-align: center;
+        padding: 3px;
+        background-color: DarkSalmon;
+        color: white;
+        }
+    </style>
+</head>
+<footer>
+  <p>Copyright @  <script>
+                    document.write(new Date().getFullYear());
+                  </script> Ahmed KONE<br>
+</footer>
